ed7d353f4b2b8a7a0f53c344bfc73f02c90ecc47
Kyndallmae <kyndallmae@gmail.com>
2023-11-22 17:42:47 -0600

    modified the schema to create tables for each item

--- db/schema.sql
+++ db/schema.sql
@@ -5,17 +5,33 @@ USE companydeets_db;
 
 
 CREATE TABLE department (
-    id INT PRIMARY KEY NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles (
-    id INT PRIMARY KEY NOT NULL,
-    title VARCHAR(30)NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+-- role table
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL (10,0) NOT NULL,
+    department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+-- employee table
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE CASCADE,
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
     ON DELETE SET NULL
 );
 
